bbb31431506aa066380188ad4a00a3821a157266
viswanathan.tj <viswanathan.tj@zohocorp.com>
2023-01-05 22:52:00 +0530

    Updated credentials with environ variables

--- pm/__init__.py
+++ pm/__init__.py
@@ -2,12 +2,13 @@ from flask import Flask, session, redirect
 from functools import wraps
 from flask.templating import render_template
 import pymongo
+import os
 
-client = pymongo.MongoClient("mongodb+srv://viswa:5900@cluster0.pbyko.mongodb.net/test?retryWrites=true&w=majority")
+client = pymongo.MongoClient(os.environ.get('DB_URL'))
 
 def create_app():
     app = Flask(__name__)
-    app.secret_key = b'\xcc^\x91\xea\x17-\xd0W\x03\xa7\xf8J0\xac8\xc5'
+    app.secret_key = os.environ.get("SECRET_KEY")
 
     from .routes import routes
     app.register_blueprint(routes, url_prefix='/')
